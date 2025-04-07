eb66acdc4a3e97a7b91a2bb8e8e0dc633686b813
Caden <cadencasteel10@gmail.com>
2025-04-07 17:49:13 -0500

    First Commit

---
+++ src/main/java/org/omnimc/mavencentraltest/Main.java
@@ -0,0 +1,11 @@
+package org.omnimc.mavencentraltest;
+
+/**
+ * @author <a href=https://github.com/CadenCCC>Caden</a>
+ * @since ${since}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
